20cda6166d2ebad464d20becdc3d442fe96dcbce
Ryan D'souza <dsouzarc@gmail.com>
2015-04-10 20:07:44 -0400

    Huge UI changes

---
+++ PromMe/PeopleAcceptedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeopleAcceptedViewController.h
+//  PromMe
+//
+//  Created by Ryan D'souza on 4/8/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface PeopleAcceptedViewController : ViewController
+
+@end
